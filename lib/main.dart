669c7ef69e45bba39fd2f91c547fe335b8b9e702
Your Name <12130906@students.liu.edu.lb>
2024-01-02 22:38:25 +0200

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'csci410 week 11',
+        title: 'Zizo Techh',
         home: Login());
   }
 }
